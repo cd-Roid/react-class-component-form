421fec93d11b2f347c0748b598d35d714bee0b50
cd-Roid <giyawe98@gmail.com>
2021-02-22 19:04:32 +0100

    ADD: basic form validation

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,6 @@
 import logo from './reignite_logo_komplett_big.svg';
 import {Component} from 'react';
-import { TextField, Button, FormLabel, Select, MenuItem, InputLabel} from '@material-ui/core';
+import { TextField, Button, FormLabel, Select, MenuItem} from '@material-ui/core';
 
 
 class App extends Component {
@@ -11,25 +11,50 @@ class App extends Component {
       username: '', 
       email: '',
       password: '',
-      framework: ''
-     }
-  
+      framework: '',
+      userNameError: false,
+      passwordError: false,
+      emailError: false,
+      frameworkError: false,
+    }
+    
   }
   
- handleEvent = (event) => {
+  validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  
+  handleEvent = (event) => {
    //name statt id weil der Select kein id property hat 
     let target = event.target.name; 
     switch (target) {
       case "email":
         this.setState({email: event.target.value});
+        let email = this.state.email;
+        if(this.validateEmail(email)){
+          this.setState({emailError: false});
+        }else{
+          this.setState({emailError: true});
+        }
         break;
       
       case "username":
         this.setState({username: event.target.value});
+        if(this.state.username.length < 5){
+          this.setState({userNameError: true});
+        }else{
+          this.setState({userNameError: false});
+        }
         break;
       
       case "password":
         this.setState({password: event.target.value});
+        if(this.state.password.length < 5){
+          this.setState({passwordError: true});
+        }else{
+          this.setState({passwordError: false});
+        }
         break;
       
       case "framework":
@@ -39,16 +64,18 @@ class App extends Component {
       default: throw new Error("no Event passed");
     }
  }
+
  sendAlert = () =>{
    //Post request can be made here
+
    let postObject = {
      //password sollte vorher gehashed sein wenn möglich
      username: this.state.username,
      email: this.state.email,
      password: this.state.password,
      framework: this.state.framework,
-   }
-   alert(`Sent form with ${JSON.stringify(postObject)}`);
+    }
+    alert(`Sent form with ${JSON.stringify(postObject)}`);  
  }
 
   render(){
@@ -62,15 +89,21 @@ class App extends Component {
             name="username"
             label="Standard" 
             value={this.state.username} 
-            onChange={this.handleEvent}/>
+            onChange={this.handleEvent}
+            error= {this.state.userNameError}
+            helperText={this.state.userNameError ? 'Username must be longer than five characters':''}
+            required= {true}/>
           <FormLabel>E-mail</FormLabel>
           <TextField 
             id="email"
             name="email" 
             label="Filled" 
             variant="filled" 
+            required= {true}
             value={this.state.email} 
-            onChange={this.handleEvent}/>
+            onChange={this.handleEvent}
+            error= {this.state.emailError}
+            helperText={this.state.emailError ? 'Invalid E-mail':''}/>
           <FormLabel>Password</FormLabel>
           <TextField 
             name="password"
@@ -78,8 +111,11 @@ class App extends Component {
             type="password"
             label="Outlined" 
             variant="outlined" 
+            required= {true}
             value={this.state.password} 
-            onChange={this.handleEvent}/>
+            onChange={this.handleEvent}
+            error= {this.state.passwordError}
+            helperText={this.state.passwordError ? 'Password must be longer than 5 characters':''}/>
           
           <FormLabel id="label">Framework</FormLabel>
           <Select
